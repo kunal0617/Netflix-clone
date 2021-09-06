4fc5564c202356e296c570ba621d7c4037421eae
kunal0617 <kunal18a@iitg.ac.in>
2021-09-07 03:32:30 +0530

    added protected routing and if user already signed in then move to browse

---
+++ src/helpers/routes.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+//if user is signed in and we go to signin page or signup page redirect them to browse
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
+
+//only signed in user to see browse page
+export function ProtectedRoute({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: 'signin',
+                state: { from: location },
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
