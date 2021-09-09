45854cba989ef80fe717f3caf272f5b1aa25e218
kunal0617 <kunal18a@iitg.ac.in>
2021-09-10 03:44:51 +0530

    build intro page added spinner animation started browse page-did till dropdown header logo etc

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components/macro';
 import { Link as ReactRouterLink } from 'react-router-dom';
+
 export const Background = styled.div`
   display: flex;
   flex-direction: column;
   background: url(${({ src }) =>
-      src ? '../images/misc/${src}.jpg' : '../images/misc/a.jpg'})
+      src ? `../images/misc/${src}.jpg` : '../images/misc/a.jpg'})
     top left / cover no-repeat;
+
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) =>
+      dontShowOnSmallViewPort && `background: none;`}
+  }
 `;
 
 export const Container = styled.div`
@@ -24,10 +30,66 @@ export const Container = styled.div`
   }
 `;
 
+export const Link = styled.p`
+  color: white;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
+
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Dropdown = styled.div`
+  display: none;
+  background-color: black;
+  position: absolute;
+  padding: 10px;
+  width: 100px;
+  top: 32px;
+  right: 10px;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  position: relative;
+
+  button {
+    cursor: pointer;
+  }
+
+  &: hover > ${Dropdown} {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const Picture = styled.button`
+  background: url(${({ src }) => src});
+  background-size: contain;
+  border: 0;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
+
 export const Logo = styled.img`
   height: 32px;
   width: 108px;
-  margin-height: 40px;
+  margin-right: 40px;
 
   @media (min-width: 1449px) {
     height: 45px;
@@ -52,4 +114,32 @@ export const ButtonLink = styled(ReactRouterLink)`
   &:hover {
     background-color: #f40612;
   }
+`;
+
+export const Feature = styled(Container)`
+  padding: 150px 0 500px 0;
+  flex-direction: column;
+  align-items: normal;
+  width: 50%;
+
+  @media (max-width: 110px) {
+    display: none;
+  }
+`;
+
+export const Text = styled.p`
+  color: white;
+  font-size: 22px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin: 0;
+`;
+
+export const FeatureCallOut = styled.h2`
+  color: white;
+  font-size: 50px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin: 0;
+  margin-bottom: 20px;
 `;
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
-  return <p>Hello from browse!</p>;
+  //get series and films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+
+  //console.log(series);
+  //console.log(films);
+
+  //sliding option also
+  const slides = selectionFilter({ series, films });
+  //console.log(slides);
+  //pass it to here browse container clean and simple
+  return <BrowseContainer slides={slides} />;
 }
